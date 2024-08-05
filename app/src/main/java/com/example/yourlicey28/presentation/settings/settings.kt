354b13d0dc5b89d6412876bbf62d034ec7b6ed23
Mikhail Nikolaev <nmichail154@mail.ru>
2024-08-05 14:11:56 +0700

    settings.kt message for exception ui

--- app/src/main/java/com/example/yourlicey28/presentation/settings/settings.kt
+++ app/src/main/java/com/example/yourlicey28/presentation/settings/settings.kt
@@ -16,23 +16,54 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowForwardIos
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Palette
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,7 +72,10 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -54,31 +88,32 @@ import com.example.yourlicey28.ui.theme.RedLC
 import com.example.yourlicey28.ui.theme.WhiteLC
 import com.example.yourlicey28.ui.theme.monterrat
 import com.example.yourlicey28.ui.theme.roboto
+import kotlinx.coroutines.launch
 
 @Composable
 fun SettingsScreen(navController: NavController) {
     val context = LocalContext.current
+    var showDialog by remember { mutableStateOf(false) }
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .background(Color.White)
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
     ) {
-
         Text(
             text = "Настройки",
             fontSize = 22.sp,
-            fontFamily = monterrat,
             fontWeight = FontWeight.Bold,
+            fontFamily = monterrat,
             color = Color.DarkGray,
-            modifier = Modifier
-                .padding(start = 20.dp, top = 20.dp)
+            modifier = Modifier.padding(start = 20.dp, top = 20.dp)
         )
 
         Box(modifier = Modifier
             .padding(top = 30.dp, start = 20.dp)
             .clip(RoundedCornerShape(20.dp))
-            .background(WhiteLC)
+            .background(Color(0xFFFAFAFA))
             .fillMaxWidth(0.95f)
             .height(60.dp)
             .clickable {
@@ -98,12 +133,13 @@ fun SettingsScreen(navController: NavController) {
                 Text(
                     text = "Тема",
                     fontSize = 16.sp,
-                    fontFamily = roboto,
+                    fontFamily = monterrat,
                     fontWeight = FontWeight.SemiBold,
-                    color = DarkBlueLC,
+                    color = Color.DarkGray,
                     modifier = Modifier.padding(top = 15.dp, start = 10.dp)
                 )
                 Spacer(modifier = Modifier.width(150.dp))
+
                 Icon(
                     imageVector = Icons.Default.ArrowForwardIos,
                     contentDescription = "Add Icon",
@@ -113,8 +149,8 @@ fun SettingsScreen(navController: NavController) {
                         .size(20.dp)
                 )
             }
-
         }
+
         Row(
             modifier = Modifier
                 .padding(16.dp)
@@ -135,24 +171,25 @@ fun SettingsScreen(navController: NavController) {
 
             Box(
                 modifier = Modifier
-                    .size(35.dp).clip(RoundedCornerShape(10.dp))
+                    .size(35.dp)
+                    .clip(RoundedCornerShape(10.dp))
                     .background(LightBlueLC),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
                     text = "1.0",
+                    fontFamily = monterrat,
                     fontSize = 16.sp,
-                    fontFamily = roboto,
                     fontWeight = FontWeight.SemiBold,
-                    color = DarkLC,
+                    color = Color.DarkGray,
                     modifier = Modifier.align(Alignment.Center)
                 )
             }
         }
+
         Text(
             text = "Это приложение является Open Source продуктом. С удовольствием буду ждать Ваших предложений и сообщений. Буду рад любой помощи/участию в проекте!",
             fontSize = 16.sp,
-            fontFamily = roboto,
             fontWeight = FontWeight.Normal,
             color = Color.DarkGray,
             modifier = Modifier.padding(start = 20.dp, end = 10.dp)
@@ -165,7 +202,6 @@ fun SettingsScreen(navController: NavController) {
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-
             Image(
                 painter = painterResource(id = R.drawable.github),
                 contentDescription = null,
@@ -182,7 +218,9 @@ fun SettingsScreen(navController: NavController) {
                         context.startActivity(intent)
                     }
             )
+
             Spacer(modifier = Modifier.width(50.dp))
+
             Image(
                 painter = painterResource(id = R.drawable.telegrampng),
                 contentDescription = null,
@@ -204,19 +242,20 @@ fun SettingsScreen(navController: NavController) {
             fontFamily = monterrat,
             fontWeight = FontWeight.Bold,
             color = Color.DarkGray,
-            modifier = Modifier
-                .padding(start = 20.dp)
+            modifier = Modifier.padding(start = 20.dp)
         )
 
         ProjectMembersList(members = members)
 
-
         Box(
-            modifier = Modifier.padding(16.dp).clip(RoundedCornerShape(20.dp))
+            modifier = Modifier
+                .padding(16.dp)
+                .clip(RoundedCornerShape(20.dp))
                 .fillMaxWidth(0.95f)
                 .height(64.dp)
-                .background(RedLC).clickable {
-
+                .background(RedLC)
+                .clickable {
+                    openBottomSheet = true
                 },
             contentAlignment = Alignment.Center
         ) {
@@ -225,29 +264,24 @@ fun SettingsScreen(navController: NavController) {
                 fontSize = 20.sp,
                 fontFamily = monterrat,
                 fontWeight = FontWeight.Bold,
-                color = WhiteLC,
+                color = Color.White,
                 modifier = Modifier.align(Alignment.Center)
             )
         }
-
+        FeedbackFormBottomSheet(openBottomSheet) { openBottomSheet = false }
     }
 }
 
-data class ProjectMember(
-    val imageUrl: String,
-    val name: String,
-    val commitCount: Int,
-    val clickUrl: String
-)
-
-val members = listOf(
-    ProjectMember(
-        "https://avatars.githubusercontent.com/u/111734592?s=400&u=a54918d28c1e4037ad6a610c66b0424b1bb3a90f&v=4",
-        "Mikhail Nikolaev",
-        52,
-        "https://github.com/MishaNikolaev"
-    ),
-)
+@Composable
+fun ProjectMembersList(members: List<ProjectMember>) {
+    LazyRow(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        items(members) { member ->
+            MemberCard(member)
+        }
+    }
+}
 
 @Composable
 fun MemberCard(member: ProjectMember) {
@@ -255,15 +289,16 @@ fun MemberCard(member: ProjectMember) {
     Card(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .clickable {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(member.clickUrl))
+                context.startActivity(intent)
+            },
         elevation = CardDefaults.cardElevation(6.dp),
-        onClick = {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(member.clickUrl))
-            context.startActivity(intent)
-        },
+        shape = RoundedCornerShape(10.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.White
-        ),
+        )
     ) {
         Row(
             modifier = Modifier
@@ -290,14 +325,130 @@ fun MemberCard(member: ProjectMember) {
     }
 }
 
+data class ProjectMember(
+    val imageUrl: String,
+    val name: String,
+    val commitCount: Int,
+    val clickUrl: String
+)
 
+val members = listOf(
+    ProjectMember(
+        "https://avatars.githubusercontent.com/u/111734592?s=400&u=a54918d28c1e4037ad6a610c66b0424b1bb3a90f&v=4",
+        "Mikhail Nikolaev",
+        52,
+        "https://github.com/MishaNikolaev"
+    ),
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProjectMembersList(members: List<ProjectMember>) {
-    LazyRow(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        items(members) { member ->
-            MemberCard(member)
+fun FeedbackFormBottomSheet(openBottomSheet: Boolean, onDismiss: () -> Unit) {
+    var skipPartiallyExpanded by rememberSaveable { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val bottomSheetState =
+        rememberModalBottomSheetState(skipPartiallyExpanded = skipPartiallyExpanded)
+
+    if (openBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { onDismiss() },
+            sheetState = bottomSheetState,
+            containerColor = Color.White
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .background(Color.White)
+            ) {
+
+                Text(
+                    text = "Оставить отзыв",
+                    fontFamily = roboto,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Кажется, у вас что-то пошло не так. Пожалуйста, напишите нам, и мы постараемся исправить это. Мы свяжемся по указанному email адресу для уточнения деталей.",
+                    fontSize = 14.sp,
+                    fontFamily = roboto,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Email",
+                    fontFamily = roboto,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+
+                var email by remember { mutableStateOf(TextFieldValue()) }
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    placeholder = { Text("Введите email") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp)
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Text(
+                    text = "Что случилось?",
+                    fontFamily = roboto,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+
+                var description by remember { mutableStateOf(TextFieldValue()) }
+                OutlinedTextField(
+                    value = description,
+                    onValueChange = { description = it },
+                    placeholder = { Text("Когда я нажимаю \"X\" происходит \"Y\"") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                        .padding(horizontal = 8.dp)
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Box(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(20.dp))
+                        .fillMaxWidth(0.95f)
+                        .height(44.dp)
+                        .background(LightBlueLC)
+                        .clickable {
+
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Отправить",
+                        fontSize = 14.sp,
+                        fontFamily = roboto,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+                Spacer(modifier = Modifier.height(70.dp))
+            }
         }
     }
 }
